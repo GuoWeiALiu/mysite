6f02efb420fc8fd31e9ae74b637446a3d58948ac
Guowei Liu A <18042971250@189.cn>
2019-05-09 16:23:35 +0800

    通用静态文件和基础模板

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    path("", views.blog_title, ),
-    path('<int:article_id>/', views.blog_article),
-    #path('<int:article_id>/', views.blog_article, name='blog_article'),
+    path("", views.blog_title, name='blog_title'),
+    #path('<int:article_id>/', views.blog_article),
+    path('<int:article_id>/', views.blog_article, name='blog_article'),
 ]
