2b3e6536f13e53a35c5e5ff9b32ad5bcc2589380
Guowei Liu A <18042971250@189.cn>
2019-05-09 15:25:53 +0800

    查看文章内容，添加内容模板，添加url， 注意get_object_or_404学习新方法

--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,7 @@ def blog_title(request):
 
 def blog_article(request, article_id):
     #article = BlogArticles.objects.get(id=article_id)
+    # 引入get_object_or_404方法，省略代码
     article = get_object_or_404(BlogArticles, id=article_id)
     pub = article.publish
     return render(request, "blog/content.html", {"article":article, "publish":pub })
