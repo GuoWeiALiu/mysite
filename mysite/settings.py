bf6ffa72a7eef2cf91a66d37fce2d48bcb98ac34
Guowei Liu A <18042971250@189.cn>
2019-05-09 16:37:45 +0800

    更改模板设置为true以便admin界面显示

--- mysite/settings.py
+++ mysite/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')]
         ,
-        'APP_DIRS': False,
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -120,6 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
+STATICFILES_DIRS = ( 
     os.path.join(BASE_DIR, "static"),
 )
