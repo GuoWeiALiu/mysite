92d02332083b6e19ea14d3574db37a2e46470b28
Guowei Liu A <18042971250@189.cn>
2019-05-14 13:52:59 +0800

    登陆完成

--- account/views.py
+++ account/views.py
@@ -1,5 +1,24 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from account.forms import LoginForm
+
+
 def user_login(request):
-    return None
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            cd = login_form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user:
+                login(request, user)
+                return HttpResponse("Wellcom you, You have been login successfully")
+            else:
+                return HttpResponse("Sorry, your username or passwd is not right")
+        else:
+            return HttpResponse("Invalid login")
+    else:
+        login_form = LoginForm()
+        return render(request, "account/login.html", {'form': login_form})
