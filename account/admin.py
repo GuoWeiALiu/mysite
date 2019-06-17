d002d1f9588219b7018ba032f16a6bdff53601d5
Guowei Liu A <18042971250@189.cn>
2019-06-17 17:56:23 +0800

    2最后对个人信息进行管理

--- account/admin.py
+++ account/admin.py
@@ -5,8 +5,8 @@ from .models import UserProfile
 
 
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'birth', 'phone')
-    list_filter = ("phone",)
+    list_display = ('user', 'birth', 'phone', 'school', "company", "profession", "address", "aboutme", "photo")
+    list_filter = ("phone", 'school', "company", "profession")
 
 
 admin.site.register(UserProfile, UserProfileAdmin)
